ed76e892a9cf2b2913dab0649017050513eb5f73
annarodrigues2 <anna.rodrigues@minervafoods.com>
2025-03-31 20:36:40 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoAulas/Program.cs
@@ -0,0 +1,9 @@
+class Program { 
+    static void Main(string[] args)
+    {
+        int numero1 = 2;
+        int numero2 = 4;
+        int Soma = numero1 + numero2;
+        Console.WriteLine("Soma:" + Soma); 
+    }
+}
