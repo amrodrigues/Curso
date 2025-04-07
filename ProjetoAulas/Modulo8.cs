a30f21f68567d6c422d64893e265b450e3fd882d
annarodrigues2 <anna.rodrigues@minervafoods.com>
2025-04-07 20:53:52 -0300

    Herança

--- ProjetoAulas/Modulo8.cs
+++ ProjetoAulas/Modulo8.cs
@@ -1,18 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Cadastro
 {
+    public static class Calculos
+    {
+        public static int SomarNumeros(int a, int b)
+        { return a * b; }
+    }
     public class Produto
     {
+        private int Id;
+
         public string Descricao { get; set; }
+
+        //public readonly int Estoque;
+        public int Estoque { get; }
+        public Produto()
+        {
+            Estoque = 1;
+        }
+        
         public void ImprimirDescricao()
         {
 
-            Console.WriteLine( Descricao );
+            Console.WriteLine(Getid()+ " - " + Descricao);
+        }
+
+        public void Setid(int id)
+            { Id = id; }
+
+        public int Getid() { return Id; }
+
+    }
+
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Endereco { get; set; }
+        public string Cidade { get; set; }
+        public string Cep { get; set; }
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Código: " + Id);
+            Console.WriteLine("Endereco: " + Endereco);
+            Console.WriteLine("Cidade: " +Cidade);
+            Console.WriteLine("Cep: " + Cep);
+        }
+    
+    }
+
+    public class PessoaFisica : Pessoa
+    {
+        public string CPF { get; set; }
+
+        public void ImprimirCPF()
+        { Console.WriteLine("CPF: " + CPF); }
+    }
+
+    public class Funcionario : PessoaFisica
+    { 
+        public string Matricula {  get; set; }
+
+        public void ImprimirMatricula()
+        {
+            Console.WriteLine("Matricula: " + Matricula);
         }
     }
+
 }
--- ProjetoAulas/Program.cs
+++ ProjetoAulas/Program.cs
@@ -3,10 +3,55 @@ public class Program
 
     static void Main(string[] args)
     {
-        //  Console.WriteLine("Hello Word");
+
+        // AulaClasse();
+        AulaHeranca();
+        AulaHeranca2();
+    }
+
+    private static void AulaClasse()
+    {
         var produto = new Cadastro.Produto();
+        produto.Setid(1);
         produto.Descricao = "Teclado";
+
         produto.ImprimirDescricao();
+        Console.WriteLine(produto.Getid());
+
+        Console.WriteLine(produto.Estoque);
+
+        var resultado = Cadastro.Calculos.SomarNumeros(2, 3);
+        Console.WriteLine(resultado);
+    }
+
+    private static void AulaHeranca()
+    {
+        var pessoaFisica = new Cadastro.PessoaFisica();
+        pessoaFisica.Id = 13;
+        pessoaFisica.Endereco = "Rua das Paineiras, 34";
+        pessoaFisica.Cidade = "Piracicaba";
+        pessoaFisica.Cep = "23420-230";
+        pessoaFisica.CPF = "15356565646";
+        pessoaFisica.ImprimirDados();
+        pessoaFisica.ImprimirCPF();
+
+    }
+
+    private static void AulaHeranca2()
+    {
+        var funcionario = new Cadastro.Funcionario();
+        funcionario.Id = 123;
+        funcionario.Endereco = "Rua das Paineiras, 34";
+        funcionario.Cidade = "Piracicaba";
+        funcionario.Cep = "23420-230";
+        funcionario.CPF = "15356565646";
+        funcionario.Matricula = "46465";
+        funcionario.ImprimirMatricula();
+        funcionario.ImprimirDados();
+        funcionario.ImprimirCPF();
+        
+
+
 
     }
 
