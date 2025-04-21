87c3f71bb6098e333bb950a5b7afe43989890f7b
annarodrigues2 <anna.rodrigues@minervafoods.com>
2025-04-21 12:34:23 -0300

    Projeto APP Cliente

---
+++ APPCliente/ClienteRepositorio.cs
@@ -0,0 +1,153 @@
+﻿using Cadastro;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio
+{
+    public class ClienteRepositorio
+    {
+        public List<Cliente> clientes = new List<Cliente>();
+
+        public void GravarDadosClientes()
+        {
+            string json = JsonConvert.SerializeObject(clientes);
+            File.WriteAllText("clientes.txt", json);  
+        }
+
+        public void LerDadosClientes()
+        {
+            if (File.Exists("Clientes.txt"))
+            {
+                var dados = File.ReadAllText("clientes.txt");
+           // string json = JsonConvert.SerializeObject(clientes);
+            var clientesArquivo = JsonConvert.DeserializeObject<List<Cliente>>(dados);
+
+            clientes.AddRange(clientesArquivo);
+
+            }
+
+        }
+
+        public void ImprimirCliente(Cliente cliente)
+        {
+            Console.WriteLine("Id................:" + cliente.Id);
+            Console.WriteLine("Nome..............:" + cliente.Name);
+            Console.WriteLine("Desconto..........:" + cliente.Desconto);
+            Console.WriteLine("Data Nascimento...:" + cliente.DataNascimento);
+            Console.WriteLine("Data Cadastro.....:" + cliente.CadastradoEm);
+            Console.WriteLine("-------------------------------------------");
+
+        }
+
+        public void ExibirClientes()
+        {
+            Console.Clear();
+            foreach (var cliente in clientes)
+            {
+                ImprimirCliente (cliente);
+            }
+
+            Console.ReadKey();
+        }
+
+        public void CadastrarCliente()
+        {
+            Console.Clear();
+
+            Console.Write("Nome do Cliente:");
+            var nome = Console.ReadLine();
+            Console.Write(Environment.NewLine);
+
+            Console.Write("Data de Nascimento:");
+            var dataNascimento = DateOnly.Parse(Console.ReadLine());
+            Console.Write(Environment.NewLine);
+
+            Console.Write("Desconto:");
+            var desconto = decimal.Parse(Console.ReadLine());
+            Console.Write(Environment.NewLine);
+
+            var cliente = new Cliente();
+            cliente.Id = clientes.Count + 1;
+            cliente.Name = nome;
+            cliente.DataNascimento = dataNascimento;
+            cliente.Desconto = desconto;
+            cliente.CadastradoEm = DateTime.Now;
+
+            clientes.Add(cliente);
+
+            Console.WriteLine("Cliente cadastrado com sucesso!");
+            ImprimirCliente(cliente);
+            Console.ReadKey();  
+        }
+
+        public void EditarCliente()
+        {
+            Console.Clear();
+            Console.Write("Informe o código do cliente:");
+            var codigo = Console.ReadLine();
+
+            var cliente = clientes.FirstOrDefault(p => p.Id== int.Parse(codigo));
+
+            if (cliente == null) 
+            {
+                Console.WriteLine("Cliente não encontrado!");
+                Console.ReadKey();
+                return;
+            }
+
+            ImprimirCliente(cliente);
+
+            Console.Write("Nome do Cliente:");
+            var nome = Console.ReadLine();
+            Console.Write(Environment.NewLine);
+
+            Console.Write("Data de Nascimento:");
+            var dataNascimento = DateOnly.Parse(Console.ReadLine());
+            Console.Write(Environment.NewLine);
+
+            Console.Write("Desconto:");
+            var desconto = decimal.Parse(Console.ReadLine());
+            Console.Write(Environment.NewLine);
+
+            cliente.Name = nome;
+            cliente.DataNascimento = dataNascimento;
+            cliente.Desconto = desconto;
+            cliente.CadastradoEm = DateTime.Now;
+
+
+            Console.WriteLine("Cliente alterado com sucesso!");
+            ImprimirCliente(cliente);
+            Console.ReadKey();
+
+        }
+
+        public void ExcluirCliente()
+        {
+            Console.Clear();
+            Console.Write("Informe o código do cliente:");
+            var codigo = Console.ReadLine();
+
+            var cliente = clientes.FirstOrDefault(p => p.Id == int.Parse(codigo));
+
+            if (cliente == null)
+            {
+                Console.WriteLine("Cliente não encontrado!");
+                Console.ReadKey();
+                return;
+            }
+
+            ImprimirCliente(cliente);
+
+            clientes.Remove(cliente);
+
+            Console.WriteLine("Cliente excluído com sucesso!");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ APPCliente/Program.cs
@@ -0,0 +1,89 @@
+﻿using Repositorio;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace AppClientes;
+
+class Program
+{
+    static ClienteRepositorio _clienteRepositorio = new ClienteRepositorio();
+    static void Main(String[] args)
+    {
+        // Caso não esteja em Português
+        //var cultura = new CultureInfo("pt-BR");
+        //Thread.CurrentThread.CurrentCulture = cultura;
+        //Thread.CurrentThread.CurrentCulture = cultura;
+
+        _clienteRepositorio.LerDadosClientes();
+        while (true)
+        { 
+        Menu(); 
+
+        Console.ReadKey();
+
+        }
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine(" Cadastro de Clientes");
+        Console.WriteLine("----------------------");
+        Console.WriteLine("1 - Cadastrar Clientes");
+        Console.WriteLine("2 - Exibir Clientes");
+        Console.WriteLine("3 - Editar Clientes");
+        Console.WriteLine("4 - Excluir Clientes");
+        Console.WriteLine("5 - Sair");
+
+        EscolherOpcao();
+    }
+
+    static void EscolherOpcao()
+    {
+        Console.WriteLine("Escolha uma opção:");
+
+        var opcao = Console.ReadLine();
+
+        switch (int.Parse(opcao))
+        {
+            case 1:
+                {
+                    _clienteRepositorio.CadastrarCliente();
+                    Menu();
+                    break;
+                }
+            case 2:
+                {
+                    _clienteRepositorio.ExibirClientes();
+                    Menu();
+                    break;
+                }
+            case 3:
+                {
+                    _clienteRepositorio.EditarCliente();
+                    Menu();
+                    break;
+                }
+            case 4:
+                {
+                    _clienteRepositorio.ExcluirCliente();
+                    Menu();
+                    break;
+                }
+
+                case 5:
+                {
+                    _clienteRepositorio.GravarDadosClientes();
+                    Environment.Exit(0);
+                    break;
+
+                }
+            default:
+                {
+                    Console.Clear();
+                    Console.WriteLine("Opção Inválida");
+                    break;
+                }
+        }
+    }
+}
